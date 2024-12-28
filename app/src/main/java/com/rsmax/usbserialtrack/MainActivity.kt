21cd0e27db7c48fbaa19037b6235595dfb2fdc82
javi <fordrsmax@gmail.com>
2024-12-28 18:38:32 +0100

    add keep screen on flag

--- app/src/main/java/com/rsmax/usbserialtrack/MainActivity.kt
+++ app/src/main/java/com/rsmax/usbserialtrack/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             UsbSerialTrackTheme {
                 AppNavigation()
